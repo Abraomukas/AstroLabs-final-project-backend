42caec76f2a8531bf49e40356d5f66f1ef27ac0a
Sonoran719 <alan.gom3z@gmail.com>
2021-05-18 18:15:17 +0400

    Updated doggosModel.js and Doggos.js routes

--- models/DoggosModel.js
+++ models/DoggosModel.js
@@ -1,31 +1,49 @@
-const mongoose = require('mongoose');
+// Import mongoose
+const mongoose = require("mongoose");
 
-const DoggosSchema = new mongoose.Schema (
-    {
-        name: {
-            type: String,
-            required: true
-        },
-        breed: {
-            type: String,
-            required: true
-        },
-        color: {
-            type: String,
-            required: true
-        },
-        likes: {
-            type: String,
-            required: true
+// Creating the schema
+const DoggosSchema = new mongoose.Schema({
+  doggoName: {
+    type: String,
+    required: true,
+  },
+  doggoBreed: {
+    type: String,
+    required: true,
+  },
+  doggoAge: {
+    type: Number,
+    required: true,
+  },
+  doggoSize: {
+    type: String,
+    required: true,
+  },
+  doggoForAdoption: {
+    type: Boolean,
+    required: true,
+  },
+  doggoSocializing: {
+    type: Boolean,
+    required: true,
+  },
 
-        },
-        does_not_like: {
-            type: String,
-            required: true
-        }
-    }
-);
+  doggoTraining: {
+    type: Boolean,
+    required: true,
+  },
 
-const DoggosModel = mongoose.model('doggos', DoggosSchema)
+  doggoDating: {
+    type: Boolean,
+    required: true,
+  },
+  doggoForRehoming: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+// Create the model
+const DoggosModel = mongoose.model("Doggos", DoggosSchema);
 
 module.exports = DoggosModel;
